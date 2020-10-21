05197fe35a24ee494aeae123c97aa7540978be3d
Michael <mokchira@gmail.com>
2020-10-21 12:19:24 -0400

    added a macro to disable validation layers if NDEBUG is defined

--- painter.c
+++ painter.c
@@ -86,7 +86,11 @@ static void initStats(void)
 void painter_Init(void)
 {
     tanto_v_config.rayTraceEnabled = true;
+#ifndef NDEBUG
+    tanto_v_config.validationEnabled = true;
+#else
     tanto_v_config.validationEnabled = false;
+#endif
     tanto_d_Init();
     printf("Display initialized\n");
     tanto_v_Init();
