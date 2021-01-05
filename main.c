ef8cd7b77efe1b713a1c3053f6787458a13d7785
Michael <mokchira@gmail.com>
2021-01-05 12:49:25 -0500

    saving

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include "painter.h"
 #include "render.h"
 #include "tanto/f_file.h"
+#include "tanto/r_geo.h"
 #include <unistd.h>
+
+void writeCube(void)
+{
+    Tanto_R_Primitive cube = tanto_r_CreateCubePrim(true);
+    Tanto_F_Primitive fprim = tanto_f_CreateFPrimFromRPrim(&cube);
+    tanto_f_WritePrimitive("data/cube-cw.tnt", &fprim);
+    tanto_r_FreePrim(&cube);
+    tanto_f_FreePrimitive(&fprim);
+}
 
 int main(int argc, char *argv[])
 {
     painter_Init();
     Tanto_F_Primitive fprim;
-    tanto_f_ReadPrimitive("data/pighead.tnt", &fprim);
+    tanto_f_ReadPrimitive("data/cube-cw.tnt", &fprim);
     Tanto_R_Primitive m = tanto_f_CreateRPrimFromFPrim(&fprim);
     tanto_f_FreePrimitive(&fprim);
     r_LoadPrim(m);
